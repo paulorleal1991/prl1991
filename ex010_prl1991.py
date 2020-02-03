823a6987ca99e4de4d092a8f1e345502db37241e
paulorleal1991 <60240604+paulorleal1991@users.noreply.github.com>
2020-02-03 01:36:37 -0400

    Conversor de moedas
    Exercícios Python  simples conversor real dólar mediante input cotação do dia

---
+++ ex010_prl1991.py
@@ -0,0 +1,26 @@
+'___Curso_de_Python___'
+'___Aluno_Paulo_R_Leal___'
+'___Exercicio_010____'
+print('')
+print('_' *40)
+print('Conversor de Moeda')
+print('_' *40)
+print('')
+r = float(input('Digite o Valor em Reais: R$ '))
+print('-' *35)
+d = float(input('Digite a Cotação do Dolar: R$'))
+print('-' *35)
+print('')
+v = r / d
+print('O Valor em Dolar é:\n')
+print('#' *35)
+print('${:^.2f}'.format(v))
+print('#' *35)
+print('')
+print('-' *35)
+print('Paulo R Leal 2020 02 02')
+print('paulorleal011291@hotmail.com')
+print('-' *35)
+print('')
+print('_' *40)
+
