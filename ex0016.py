88200e59ed01411af1017e0f49ca45056e7ee2b7
PAULO ROBERTO LEAL <60240604+paulorleal1991@users.noreply.github.com>
2020-02-07 10:47:53 -0400

    Python exercio de importacao de Bliblioteca
    ex import math

---
+++ ex0016.py
@@ -0,0 +1,14 @@
+'___Curso_Python___'
+'___Aluno_Paulo_R_Leal___'
+'___Exercicio_016___'
+print('Exercicio 016 uso de Blibliotecas ')
+print('')
+from math import sqrt
+s = float(input('Digite um número \n'))
+raiz = sqrt(s)
+print('A raiz de {:.0f} sera {}\n'.format(s, raiz))
+
+print(s)
+import random
+n = random.randint(1, 100)
+print('O ganhador é o numero:{} \n'.format(n))
